e947083e4839e4e8110d647a86cf1f1ff2e6567a
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-01-04 22:31:06 +0100

    fix: focused reactivity

--- widget/bar/focus.tsx
+++ widget/bar/focus.tsx
@@ -1,6 +1,6 @@
 import AstalHyprland from "gi://AstalHyprland"
 import Apps from "gi://AstalApps"
-import { createBinding, createComputed } from "ags"
+import { createBinding, createComputed, createState } from "ags"
 
 type Props = {
   hyprland: AstalHyprland.Hyprland
@@ -14,15 +14,42 @@ const apps = new Apps.Apps({
 
 export default function Focus({ hyprland }: Props) {
   const client = createBinding(hyprland, "focusedClient")
+  let [clientTitle, setClientTitle] = createState(hyprland.focusedClient?.title)
+  let [clientClass, setClientClass] = createState(hyprland.focusedClient?.class)
+  let clientTitleUnsub = () => {}
+  let clientClassUnsub = () => {}
+
+  const updateClientBindings = () => {
+    let c = client()
+    let bindingTitle = createBinding(c, "title")
+    let bindingClass = createBinding(c, "class")
+
+    clientTitleUnsub()
+    clientClassUnsub()
+
+    clientTitleUnsub = bindingTitle.subscribe(() => {
+      setClientTitle(bindingTitle())
+    })
+    clientClassUnsub = bindingClass.subscribe(() => {
+      setClientClass(bindingClass())
+    })
+
+    setClientTitle(bindingTitle())
+    setClientClass(bindingClass())
+  }
+
+  client.subscribe(updateClientBindings)
+  updateClientBindings()
+
   const clientName = createComputed(() => {
-    const className = client()?.class
+    const className = clientClass()
     if (!className) return "?"
 
     const appInfos = apps.fuzzy_query(className)[0]
     return appInfos ? appInfos.name : "?"
   })
   const clientDetail = createComputed(() => {
-    let title = client()?.title || ""
+    let title = clientTitle() || ""
 
     if (title.endsWith(` - ${clientName()}`)) {
       title = title.slice(0, title.length - clientName().length - 3)
